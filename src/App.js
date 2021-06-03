698637bea33475b169189eb2501811265cac5f84
Charles Raviaud <charles.raviaud@protonmail.com>
2021-06-03 13:48:07 +0200

    Setting up the first routes and redirection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
 	return <Router>
-		<Route path="/">
-			<Users />
-		</Route>
-
+		<Switch>
+			<Route path="/" exact>
+				<Users />
+			</Route>
+			<Route path="/places/new" exact>
+				<NewPlace />
+			</Route>
+			<Redirect to="/" />
+		</Switch>
 	</Router>
 }
 
