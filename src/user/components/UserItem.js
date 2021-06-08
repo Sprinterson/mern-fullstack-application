5274f4732bb313f1f8fa7692943ed623b42d0f5d
Charles <Sprinterson@users.noreply.github.com>
2021-06-08 12:12:58 +0200

    Adding a UsersList Page / Component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ‘./UserItem.css’;
+
+const UserItem = props => {
+    return <h2>Users Works!</h2>
+};
+
+export default UserItem;
---
+++ src/user/components/UsersList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersList = props => {
+    if (props.items.length === 0) {
+		return (
+			<div className="center">
+				<h2>No users found.</h2>
+			</div>
+		);    }
+	
+	return (
+		<ul>
+			{props.items.map(user => {
+				<UserItem key={user.id} id={user.id} image={user.image} name={user.name} placeCount={user.places}/>;
+			})}
+		</ul>
+	);kkkk
+};
+
+export default UsersList;
