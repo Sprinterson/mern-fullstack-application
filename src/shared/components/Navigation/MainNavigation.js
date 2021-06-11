6763c46f057138edc8da86a835dba48dd86f4575
Charles Raviaud <charles.raviaud@protonmail.com>
2021-06-11 18:40:09 +0200

    Handling the Drawer State

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader'
-import './MainNavigation.css';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
+import './MainNavigation.css';
 
 const MainNavigation = props => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    };
+
     return (
         <React.Fragment>
-            <SideDrawer>
-                <nav className="main-navigation__drawer-nav">
-                    <NavLinks />
-                </nav>
-            </SideDrawer>
+            {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+            {drawerIsOpen && (
+                <SideDrawer>
+                    <nav className="main-navigation__drawer-nav">
+                        <NavLinks />
+                    </nav>
+                </SideDrawer>
+            )}
             <MainHeader>
-                <button className="main-navigation__menu-btn">
+                <button className="main-navigation__menu-btn" onClick={openDrawer}>
                     <span />
                     <span />
                     <span />
                 </button>
                 <h1 className="main-navigation__title">
-                    <Link to='/'> YourPlaces</Link>
+                    <Link to='/'>YourPlaces</Link>
                 </h1>
                 <nav className="main-navigation__header-nav">
                     <NavLinks />
